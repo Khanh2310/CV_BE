6e893c2e69fbcebf5644b63e4c76a448960a6d6d
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-02-05 15:49:00 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly configServices: ConfigService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.configServices.get<string>('PORT');
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleAuth(@Request() req) {
+    return req.user;
   }
 }
