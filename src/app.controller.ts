5dc2c06d434783d6facf4e65f33a33da7351c8ec
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-02-07 14:05:55 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './auth/customize/customize';
 
 @Controller()
 export class AppController {
@@ -12,6 +13,7 @@ export class AppController {
     private readonly authService: AuthService,
   ) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   async handleAuth(@Request() req) {
