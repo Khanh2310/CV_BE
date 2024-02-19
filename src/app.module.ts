979c4399017dab5543ca23fc0572c6df3259e44b
quocbao <hoangquocbao650@gmail.com>
2024-02-19 23:11:57 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-
+import { CompaniesModule } from './companies/companies.module';
+import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
+        connectionFactory: (connection) => {
+          connection.plugin(softDeletePlugin);
+          return connection;
+        },
       }),
       inject: [ConfigService],
     }),
@@ -19,6 +24,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     UsersModule,
     AuthModule,
+    CompaniesModule,
   ],
 
   controllers: [AppController],
