923033e11418a656570d02a1ddbfce22d1e298b2
quocbao <hoangquocbao650@gmail.com>
2024-02-19 23:17:20 +0700

    update

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
-import { Company } from './schemas/company.schema';
+import { Company, CompanyDocument } from './schemas/company.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
-import { CompanyDocument } from '../../dist/companies/schema/company.schema';
 
 @Injectable()
 export class CompaniesService {
