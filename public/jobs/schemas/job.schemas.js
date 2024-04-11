b46598972aff374c4aee8ed5b9cce59dafa27c0a
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-04-11 11:58:23 +0700

    created schemas JOBS

--- public/jobs/schemas/job.schemas.js
+++ public/jobs/schemas/job.schemas.js
@@ -1,7 +1,91 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Job = void 0;
-class Job {
-}
-exports.Job = Job;
+exports.JobsSchema = exports.Jobs = void 0;
+const mongoose_1 = require("@nestjs/mongoose");
+let Jobs = class Jobs {
+};
+exports.Jobs = Jobs;
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Jobs.prototype, "name", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Array)
+], Jobs.prototype, "skills", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Jobs.prototype, "company", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Jobs.prototype, "location", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Number)
+], Jobs.prototype, "salary", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Jobs.prototype, "quantity", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Jobs.prototype, "level", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", String)
+], Jobs.prototype, "description", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Object)
+], Jobs.prototype, "startDate", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Object)
+], Jobs.prototype, "endDate", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Boolean)
+], Jobs.prototype, "isActive", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Object)
+], Jobs.prototype, "createdAt", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Object)
+], Jobs.prototype, "updatedAt", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Object)
+], Jobs.prototype, "isDeleted", void 0);
+__decorate([
+    (0, mongoose_1.Prop)({ type: Object }),
+    __metadata("design:type", Object)
+], Jobs.prototype, "createdBy", void 0);
+__decorate([
+    (0, mongoose_1.Prop)({ type: Object }),
+    __metadata("design:type", Object)
+], Jobs.prototype, "updatedBy", void 0);
+__decorate([
+    (0, mongoose_1.Prop)(),
+    __metadata("design:type", Object)
+], Jobs.prototype, "deletedBy", void 0);
+exports.Jobs = Jobs = __decorate([
+    (0, mongoose_1.Schema)({
+        timestamps: true,
+    })
+], Jobs);
+exports.JobsSchema = mongoose_1.SchemaFactory.createForClass(Jobs);
 //# sourceMappingURL=job.schemas.js.map
--- src/jobs/schemas/job.schemas.ts
+++ src/jobs/schemas/job.schemas.ts
@@ -1,1 +1,71 @@
-export class Job {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Date, HydratedDocument } from 'mongoose';
+
+export type JobsDocument = HydratedDocument<Jobs>;
+
+@Schema({
+  timestamps: true,
+})
+export class Jobs {
+  @Prop()
+  name: string;
+
+  @Prop()
+  skills: string[];
+
+  @Prop()
+  company: string;
+
+  @Prop()
+  location: string;
+
+  @Prop()
+  salary: number;
+
+  @Prop()
+  quantity: string; // số lượng tuyển
+
+  @Prop()
+  level: string; // Vị trí INTERN/FRESHER/JUNIOR/SENIOR
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  startDate: Date;
+
+  @Prop()
+  endDate: Date;
+
+  @Prop()
+  isActive: boolean;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+
+  @Prop()
+  isDeleted: Date;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop()
+  deletedBy: {
+    _id: mongoose.Types.ObjectId;
+    email: string;
+  };
+}
+
+export const JobsSchema = SchemaFactory.createForClass(Jobs);
