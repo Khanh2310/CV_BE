a0ffd2ca16766c880ae0660a374f4a565e0932b5
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-04-12 14:26:24 +0700

    delete file temp in public

--- public/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AppService } from './app.service';
-import { AuthService } from './auth/services/auth.service';
-export declare class AppController {
-    private readonly appService;
-    private authService;
-    constructor(appService: AppService, authService: AuthService);
-}
--- public/utils/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const HTTP_MESSAGE: {
-    [key: number]: string;
-};
