77e6d18a581a13741dd6b5e9e75a53dc17e7a0de
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-04-23 16:49:45 +0700

    update auth guards

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,13 @@
 import {
   ExecutionContext,
+  ForbiddenException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../decorator';
+import { Request } from 'express';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(private reflector: Reflector) {
@@ -16,16 +18,34 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       context.getHandler(),
       context.getClass(),
     ]);
+
     if (isPublic) {
       return true;
     }
     return super.canActivate(context);
   }
 
-  handleRequest(err, user) {
+  handleRequest(err, user, info, context: ExecutionContext) {
+    const request: Request = context.switchToHttp().getRequest();
+
     if (err || !user) {
       throw err || new UnauthorizedException('Token invalid');
     }
+
+    const targetMethod = request.method;
+    const targetEndPoint = request?.route.path;
+
+    const permissions = user?.permissions ?? [];
+    const isExits = permissions.find(
+      (permission) =>
+        targetMethod === permission.method &&
+        targetEndPoint === permission.apiPath,
+    );
+
+    if (!isExits) {
+      throw new ForbiddenException('You do not have access');
+    }
+
     return user;
   }
 }
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,10 +3,14 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IUser } from 'src/users/types';
+import { RolesService } from 'src/roles/services';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private rolesService: RolesService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -16,12 +20,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: IUser) {
     const { _id, name, email, role } = payload;
+    const userRole = role as unknown as { _id: string; name: string };
+
+    const findRole = (await this.rolesService.findOne(userRole._id)).toObject();
     // req.user
     return {
       _id,
       name,
       email,
       role,
+      permissions: findRole?.permissions ?? [],
     };
   }
 }
