054bcbaf16a8fd5ffd4d0e78682491ba68e531ab
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-04-26 16:47:40 +0700

    update mail

--- src/mail/controllers/mail.controller.ts
+++ src/mail/controllers/mail.controller.ts
@@ -2,22 +2,37 @@ import { Controller, Get } from '@nestjs/common';
 import { MailService } from '../services/mail.service';
 import { Public } from 'src/auth/decorator';
 import { MailerService } from '@nestjs-modules/mailer';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Subscriber, SubscriberDocument } from 'src/subscribers/schemas';
+import { Jobs, JobsDocument } from 'src/jobs/schemas';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Controller('mail')
 export class MailController {
   constructor(
     private readonly mailService: MailService,
     private mailerService: MailerService,
+    @InjectModel(Subscriber.name)
+    private subscriberModel: SoftDeleteModel<SubscriberDocument>,
+    @InjectModel(Jobs.name)
+    private jobsModel: SoftDeleteModel<JobsDocument>,
   ) {}
 
   @Get()
   @Public()
   async handleTestEmail() {
+    const subscribers = await this.subscriberModel.find({});
+    for (const subs of subscribers) {
+      const subsSkills = subs.skills;
+      const jobWithMatchingSkills = await this.jobsModel.find({
+        skills: { $in: subsSkills },
+      });
+    }
     await this.mailerService.sendMail({
-      to: 'hoangquocbao150@gmail.com',
+      to: 'example@gmail.com',
       from: 'Support Team <support@example.com>',
-      subject: 'Welcome to nice',
-      template: 'job',
+      subject: 'Welcome to Nice App! Comfirm your Email',
+      template: 'new-job',
     });
   }
 }
--- src/mail/index.ts
+++ src/mail/index.ts
@@ -5,6 +5,9 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Subscriber, SubscriberSchema } from 'src/subscribers/schemas';
+import { Jobs, JobsSchema } from 'src/jobs/schemas';
 
 @Module({
   imports: [
@@ -14,7 +17,7 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
           host: configService.get<string>('EMAIL_HOST'),
           secure: false,
           auth: {
-            user: configService.get<string>('EMAIL_USER'),
+            user: configService.get<string>('aaa'),
             pass: configService.get<string>('EMAIL_PASSWORD'),
           },
         },
@@ -30,6 +33,16 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
       }),
       inject: [ConfigService],
     }),
+    MongooseModule.forFeature([
+      {
+        name: Subscriber.name,
+        schema: SubscriberSchema,
+      },
+      {
+        name: Jobs.name,
+        schema: JobsSchema,
+      },
+    ]),
   ],
   controllers: [MailController],
   providers: [MailService],
